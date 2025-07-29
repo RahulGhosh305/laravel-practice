ff5f89083911425bfde2f42678dd6263ebaed8bc
Rahul Ghosh <rahul.info305@gmail.com>
2025-07-29 17:39:09 +0600

    component

---
+++ resources/views/components/message-banner.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <span class='{{$class}}'>{{$msg}}</span>
+    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,30 @@
+<!-- Component Insert -->
+<x-message-banner msg="User login Successfully" class='success' />
+<x-message-banner msg="User sign-in Successfully" class='success'/>
+
+<br>
+<x-message-banner msg="Password Not Correct" class='error'/>
+
 <h1>Welcome Laravel</h1>
 <a href="/about">About Page</a>
 <a href="/details/123">Details Page</a>
+
+<style>
+    .success {
+        background: lightgreen;
+        color: green;
+        padding: 3px 10px;
+        border-radius:2px;
+        display:inline-block;
+        margin:10px;
+    }
+
+    .error {
+        background: red;
+        color: white;
+        padding: 3px 10px;
+        border-radius:2px;
+        display:inline-block;
+        margin:10px;
+    }
+</style>
